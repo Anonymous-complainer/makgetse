71dfa77af74b171f3ae0a02383bbbd6f22ed5a75
Silent Advocate <Anonymous.complainer2000@gmail.com>
2025-04-19 19:22:02 +0200

    Update allhandler.js

--- js/allhandler.js
+++ js/allhandler.js
@@ -1,5 +1,5 @@
 
-let line="https://kertelizerkat.pythonanywhere.com/"
+let line="http://sculpin-charming-directly.ngrok-free.app"
 // line="http://localhost:5700"
 // let theme_m='Dark'
 // localStorage.setItem('theme_+-----', theme_m)
